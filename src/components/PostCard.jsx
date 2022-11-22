95e0bc021c34137fbddbdf2e1b1596f6e080708b
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-22 20:38:11 +0100

    render post data

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,28 +1,57 @@
+import { useEffect, useState } from "react";
 import CategoryTag from "./CategoryTag";
 import "./PostCard.css";
 
-const PostCard = () => {
+const PostCard = ({ postData, handleTagClick }) => {
+  const [tagArrays, setTagArrays] = useState([]);
+  useEffect(() => {
+    setTagArrays((prevTagArrays) => {
+      return [
+        ...prevTagArrays,
+        postData.role,
+        postData.level,
+        ...postData.languages,
+        ...postData.tools,
+      ];
+    });
+    return () => {
+      setTagArrays([]);
+    };
+  }, []);
+
   return (
-    <div className="post-card">
+    <div
+      className={postData.featured ? "post-card featured-post" : "post-card"}
+    >
       <div className="post-img">
-        <img src="/images/account.svg" alt="post-img" />
+        <img src={`/images/${postData.logo}`} alt="post-img" />
       </div>
       <div className="post-data">
         <div className="title-tags">
-          <h3 className="post-title">Account</h3>
-          <span className="tag new">NEW!</span>
-          <span className="tag featured">FEATURED</span>
+          <h3 className="company">{postData.company}</h3>
+          {postData.new ? <span className="tag new">NEW!</span> : ""}
+          {postData.featured ? (
+            <span className="tag featured">FEATURED</span>
+          ) : (
+            ""
+          )}
         </div>
-        <h2 className="role">Senior Frontend Developer</h2>
+        <h2 className="position">{postData.position}</h2>
         <div className="additional-info">
-          <span className="info posted-at">1d ago</span>
-          <span className="info contract">Full time</span>
-          <span className="info location">USA only</span>
+          <span className="info posted-at">{postData.postedAt}</span>
+          <span className="info contract">{postData.contract}</span>
+          <span className="info location">{postData.location}</span>
         </div>
       </div>
       <hr className="divider" />
       <div className="post-category">
-        <CategoryTag tagName="HTML" />
+        {tagArrays.map((tag) => (
+          <CategoryTag
+            key={tag}
+            tagName={tag}
+            handleClick={() => handleTagClick(tag)}
+          />
+        ))}
       </div>
     </div>
   );
