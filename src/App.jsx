949913eed836a1fbbc71cdb3e34f8d0a5181166f
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-22 20:39:07 +0100

    create functions to handle tag clicks/ set filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import "./App.css";
 import PostCard from "./components/PostCard";
+import data from "../data.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [jobPostings, setJobPostings] = useState(data);
+  const [filters, setFilters] = useState([]);
+
+  const handleTagClick = (tag) => {
+    if (filters.includes(tag)) {
+      return;
+    }
+    setFilters((prevFilters) => {
+      return [...prevFilters, tag];
+    });
+  };
+
+  useEffect(() => {
+    console.log(filters);
+  }, [filters]);
+
+  const renderJobs = jobPostings.map((post) => (
+    <PostCard key={post.id} postData={post} handleTagClick={handleTagClick} />
+  ));
   return (
     <div className="App">
       <header className="bg-top">
         <picture>
           <source
             media="(max-width: 480px)"
-            srcset="/images/bg-header-mobile.svg"
+            srcSet="/images/bg-header-mobile.svg"
           />
           <img src="/images/bg-header-desktop.svg" alt="bg-header" />
         </picture>
       </header>
       <main className="main">
-        <div className="container">
-          <PostCard />
-        </div>
+        <div className="container">{renderJobs}</div>
       </main>
     </div>
   );
